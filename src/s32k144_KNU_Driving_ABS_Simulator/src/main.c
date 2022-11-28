50a42957bbeb744661f89a261657a4fbf4ccaa9f
Kyung <boris3853@naver.com>
2022-11-28 14:01:53 +0900

    Reconstructed Proejct

--- src/s32k144_KNU_Driving_ABS_Simulator/src/main.c
+++ src/s32k144_KNU_Driving_ABS_Simulator/src/main.c
@@ -2,17 +2,17 @@
 
 void GPIO_PORT_INIT()
 {
-    // GPIO PCC 설정 - CGC 마스킹
+    // GPIO PCC Setting - CGC Masking
     PCC->PCCn[PCC_PORTA_INDEX] |= PCC_PCCn_CGC_MASK;
 
-    /* GPIOA PDDR 설정
+    /* GPIOA PDDR Setting
     // INPUT: PA6 PA7 PA8 PA9
     // OUTPUT: PA11 PA12 PA13
     */
     PTA->PDDR &= ~((1<<6)|(1<<7)|(1<<8)|(1<<9));
     PTA->PDDR |= (1<<11)|(1<<12)|(1<<13);
 
-    // PORTA: PA6 PA7 PA8 PA9 PA11 PA12 PA13 MUX 설정
+    // PORTA: PA6 PA7 PA8 PA9 PA11 PA12 PA13 MUX Setting
     PORTA->PCR[6] |= PORT_PCR_MUX(1);
     PORTA->PCR[7] |= PORT_PCR_MUX(1);
     PORTA->PCR[8] |= PORT_PCR_MUX(1);
